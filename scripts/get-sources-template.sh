58b28d67378b2b5c96d1bd7209d8ab87f6f8190d
heuristack <heuristack@gmail.com>
2016-01-17 23:38:02 +0800

    template to synchronize remote source to local

---
+++ scripts/get-sources-template.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+##===----------------------------------------------------------------------===##
+#
+#
+#        template to get remote sources to local building environment
+#
+#
+##===----------------------------------------------------------------------===##
+cd $HOME
+
+##==
+#
+# parameters to specify remote host, remote place and local place
+#
+##==
+userid=
+remote=
+origin=
+target=
+
+##==
+#
+# function to remote synchronize a source tree to proper place
+#
+##==
+function get_remote_sources_syn()
+{
+    tree=$1
+    rsync $userid@$remote:$origin/$tree $target
+}
+
+##==
+#
+# synchronize the 'tree' by calling the function above
+#
+##==
+# get_remote_sources_sync tree
+
+##===----------------------------------------------------------------------===##
+
