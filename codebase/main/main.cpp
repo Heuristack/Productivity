4da6a7cf6c514e86b6eebd42471d2dc720cb47fc
heuristack <heuristack@gmail.com>
2016-01-27 12:30:11 +0800

    main: add universe main

---
+++ codebase/main/main.cpp
@@ -0,0 +1,24 @@
+//===----------------------------------------------------------------------===//
+//
+//
+//                       integration of libraries
+//
+//
+//===----------------------------------------------------------------------===//
+
+
+#include <iostream>
+#include "boost/asio.hpp"
+#include "wiredtigher/wiredtiger.h"
+#include "berkeleydb/db_cxx.h"
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    boost::asio::io_service service;
+    cout << "Hello, World!" << endl;
+}
+
+//===----------------------------------------------------------------------===//
